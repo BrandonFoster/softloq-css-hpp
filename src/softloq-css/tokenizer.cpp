c952d7ccd41e6c5e92ea280fee5f3a0abe135a90
Brandon Foster <Brandon.Foster4525@outlook.com>
2024-12-02 00:42:13 -0500

    Update tokenizer.cpp

--- src/softloq-css/tokenizer.cpp
+++ src/softloq-css/tokenizer.cpp
@@ -2,5 +2,120 @@
 
 namespace Softloq::CSS
 {
-
+    SOFTLOQ_CSS_API const bool Tokenizer::consumeTokens(const std::string &css_text, TokenList &tokens)
+    {
+        return false;
+    }
+    SOFTLOQ_CSS_API Token *Tokenizer::consumeToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API CommentToken *Tokenizer::consumeCommentToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API WhitespaceToken *Tokenizer::consumeWhitespaceToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API StringToken *Tokenizer::consumeStringToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API Token *Tokenizer::consumeNumericToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API NumberToken *Tokenizer::consumeNumberToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API PercentageToken *Tokenizer::consumePercentageToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API DimensionToken *Tokenizer::consumeDimensionToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API Token *Tokenizer::consumeIdentLikeToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API IdentToken *Tokenizer::consumeIdentToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API FunctionToken *Tokenizer::consumeFunctionToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API URLToken *Tokenizer::consumeURLToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API BadURLToken *Tokenizer::consumeBadURLToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API AtKeywordToken *Tokenizer::consumeAtKeywordToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API HashToken *Tokenizer::consumeHashToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API LParenToken *Tokenizer::consumeLeftParenToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API RParenToken *Tokenizer::consumeRightParenToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API CommaToken *Tokenizer::consumeCommaToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API CDOToken *Tokenizer::consumeCDOToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API CDCToken *Tokenizer::consumeCDCToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API SemicolonToken *Tokenizer::consumeSemicolonToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API ColonToken *Tokenizer::consumeColonToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API LSquareToken *Tokenizer::consumeLeftSquareBracketToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API RSquareToken *Tokenizer::consumeRightSquareBracketToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API LCurlyToken *Tokenizer::consumeLeftCurlyBracketToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API RCurlyToken *Tokenizer::consumeRightCurlyBracketToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API EOFToken *Tokenizer::consumeEOFToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
+    SOFTLOQ_CSS_API DelimToken *Tokenizer::consumeDelimToken(const std::string &css_text)
+    {
+        return nullptr;
+    }
 }
