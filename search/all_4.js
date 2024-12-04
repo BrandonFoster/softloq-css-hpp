91745e1777c763e8f92723bdc3d0c3def3b0fa97
BrandonFoster <Brandon.Foster4525@outlook.com>
2024-12-04 12:55:06 +0000

    Deploying to docs from @ BrandonFoster/softloq-css-hpp@2cbc00e5eb482608373d9df86d5f746c1abb2a81 🚀

--- search/all_4.js
+++ search/all_4.js
@@ -7,11 +7,12 @@ var searchData=
   ['commatoken_4',['CommaToken',['../d8/da9/classSoftloq_1_1CSS_1_1CommaToken.html',1,'Softloq::CSS']]],
   ['commenttoken_5',['CommentToken',['../db/d26/classSoftloq_1_1CSS_1_1CommentToken.html',1,'Softloq::CSS']]],
   ['convertcodepointtoutf8_6',['convertCodepointToUTF8',['../d0/df5/unicode_8hpp.html#ae4730be8ad280c96596262feec6bbe81',1,'Softloq::Unicode']]],
-  ['convertutf8tocodepoint_7',['convertUTF8ToCodepoint',['../d0/df5/unicode_8hpp.html#a10ff325be46ffb023df497eacb7bb204',1,'Softloq::Unicode']]],
-  ['css_8',['Softloq CSS',['../index.html',1,'']]],
-  ['css_20macros_20test_9',['softloq-css-macros-test',['../index.html#autotoc_md7',1,'']]],
-  ['css_20parser_20test_10',['softloq-css-parser-test',['../index.html#autotoc_md10',1,'']]],
-  ['css_20token_20test_11',['softloq-css-token-test',['../index.html#autotoc_md8',1,'']]],
-  ['css_20tokenizer_20test_12',['softloq-css-tokenizer-test',['../index.html#autotoc_md9',1,'']]],
-  ['css_20version_201_200_200_13',['Softloq CSS Version 1.0.0',['../index.html#autotoc_md0',1,'']]]
+  ['convertstringtoutf8codepointvector_7',['convertStringToUTF8CodepointVector',['../d0/df5/unicode_8hpp.html#ac97a606518035891e1e0e3e28b9d7e83',1,'Softloq::Unicode']]],
+  ['convertutf8tocodepoint_8',['convertUTF8ToCodepoint',['../d0/df5/unicode_8hpp.html#a4219248a19f0b503eb0e9c70e39bb80f',1,'Softloq::Unicode']]],
+  ['css_9',['Softloq CSS',['../index.html',1,'']]],
+  ['css_20macros_20test_10',['softloq-css-macros-test',['../index.html#autotoc_md7',1,'']]],
+  ['css_20parser_20test_11',['softloq-css-parser-test',['../index.html#autotoc_md10',1,'']]],
+  ['css_20token_20test_12',['softloq-css-token-test',['../index.html#autotoc_md8',1,'']]],
+  ['css_20tokenizer_20test_13',['softloq-css-tokenizer-test',['../index.html#autotoc_md9',1,'']]],
+  ['css_20version_201_200_200_14',['Softloq CSS Version 1.0.0',['../index.html#autotoc_md0',1,'']]]
 ];
